a2579ce6def5828dfb2c06e6c447e84f7d90d9b1
zhangyifei <a758922095@163.com>
2019-05-08 20:00:42 +0800

    超英预热项目完成

--- config/sidebarConf/HybridApp/Dclound/DcloudAction.js
+++ config/sidebarConf/HybridApp/Dclound/DcloudAction.js
@@ -1,5 +1,5 @@
 const utils = require('../../../../utils/index');
-const children = ['超英电影预热项目',  ]
+const children = ['超英电影预热项目', '新项目' ]
 module.exports =[
-    utils.genSidebar('项目实战', children, false ),
+    utils.genSidebar('项目实战', children, true ),
 ];
