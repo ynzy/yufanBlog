9d43ef7f5410e36945f3388615815c3ac9bed9f0
zhangyong <zhangyong@163.com>
2019-06-27 16:52:48 +0800

    案例无人点餐系统

--- config/sidebarConf/Angular/Basic/basic.js
+++ config/sidebarConf/Angular/Basic/basic.js
@@ -1,6 +1,8 @@
 const utils = require('../../../../utils/index');
 const children = ['创建应用', '创建组件及模板','angular表单',
-'案例todoList','案例京东app搜索缓存数据功能','Dom 操作以及@ViewChild','组件通信','生命周期函数','异步获取数据Rxjs基础使用','Angular数据交互','路由和导航',]
+'案例todoList','案例京东app搜索缓存数据功能','Dom 操作以及@ViewChild',
+'组件通信','生命周期函数','异步获取数据Rxjs基础使用',
+'Angular数据交互','路由和导航','案例无人点餐系统','自定义模块','路由模块懒加载']
 module.exports =[
     utils.genSidebar('构建应用基础', children, false ),
 ];
