79ed91ddb7b9405231c03c788df5710c6701b2a7
zhangyong <zhangyong@163.com>
2019-07-04 16:52:16 +0800

    函数的扩展

--- config/sidebarConf/Javascript/ECMAScript6/ECMAScript6.js
+++ config/sidebarConf/Javascript/ECMAScript6/ECMAScript6.js
@@ -1,5 +1,7 @@
 const utils = require('../../../../utils/index');
-const children = ['let和const命令', '变量的解构赋值',]
+const children = ['let和const命令', '变量的解构赋值','字符串的扩展','函数的扩展',
+
+]
 module.exports =[
     utils.genSidebar('ECMAScript6学习', children, false ),
 ];
