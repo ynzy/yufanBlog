0229f79de6e86bf48de27b5ed3543362e83929af
zhangyong <zhangyong@163.com>
2019-07-16 17:52:46 +0800

    Iterator和for...of循环

--- config/sidebarConf/Javascript/ECMAScript6/ECMAScript6.js
+++ config/sidebarConf/Javascript/ECMAScript6/ECMAScript6.js
@@ -1,7 +1,7 @@
 const utils = require('../../../../utils/index');
 const children = ['let和const命令', '变量的解构赋值','字符串的扩展',
 '函数的扩展','对象的扩展','对象的新增方法','Symbol','Set和Map数据结构',
-'Proxy','Promise对象'
+'Proxy','Promise对象','Iterator和for...of循环'
 
 ]
 module.exports =[
