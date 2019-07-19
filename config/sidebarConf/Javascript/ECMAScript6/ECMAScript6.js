87784b4f37fc00a82762bc3f07866ddfd2236cdf
zhangyong <zhangyong@163.com>
2019-07-19 16:35:07 +0800

    feat(markdown)async函数

--- config/sidebarConf/Javascript/ECMAScript6/ECMAScript6.js
+++ config/sidebarConf/Javascript/ECMAScript6/ECMAScript6.js
@@ -1,8 +1,8 @@
 const utils = require('../../../../utils/index');
 const children = ['let和const命令', '变量的解构赋值','字符串的扩展',
 '函数的扩展','对象的扩展','对象的新增方法','Symbol','Set和Map数据结构',
-'Proxy','Promise对象','Iterator和for...of循环','Generator函数的语法','async函数'
-
+'Proxy','Promise对象','Iterator和for...of循环','Generator函数的语法',
+'Generator函数的异步应用','async函数'
 ]
 module.exports =[
     utils.genSidebar('ECMAScript6学习', children, false ),
