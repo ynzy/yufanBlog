4433c58756a642d25879255461d9a6c841c4e409
zhangyong <zhangyong@163.com>
2019-07-23 11:24:22 +0800

    Class的基本语法

--- config/sidebarConf/Javascript/ECMAScript6/ECMAScript6.js
+++ config/sidebarConf/Javascript/ECMAScript6/ECMAScript6.js
@@ -2,7 +2,7 @@ const utils = require('../../../../utils/index');
 const children = ['let和const命令', '变量的解构赋值','字符串的扩展',
 '函数的扩展','对象的扩展','对象的新增方法','Symbol','Set和Map数据结构',
 'Proxy','Promise对象','Iterator和for...of循环','Generator函数的语法',
-'Generator函数的异步应用','async函数'
+'Generator函数的异步应用','async函数','Class的基本语法'
 ]
 module.exports =[
     utils.genSidebar('ECMAScript6学习', children, false ),
