510fcae0e33da08108d96e7cd02cb3f799362c0a
zhangyong <zhangyong@163.com>
2019-07-29 17:43:29 +0800

    Module的加载实现

--- config/sidebarConf/Javascript/ECMAScript6/ECMAScript6.js
+++ config/sidebarConf/Javascript/ECMAScript6/ECMAScript6.js
@@ -3,7 +3,7 @@ const children = ['let和const命令', '变量的解构赋值','字符串的扩�
 '函数的扩展','对象的扩展','对象的新增方法','Symbol','Set和Map数据结构',
 'Proxy','Promise对象','Iterator和for...of循环','Generator函数的语法',
 'Generator函数的异步应用','async函数','Class的基本语法','Class的继承',
-'Module的语法'
+'Module的语法','Module的加载实现'
 ]
 module.exports =[
     utils.genSidebar('ECMAScript6学习', children, false ),
