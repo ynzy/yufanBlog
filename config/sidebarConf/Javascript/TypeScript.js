1da0bc5510bea64ce140105c75a1f1cf362b3c73
zhangyong <zhangyong@163.com>
2019-07-31 17:46:18 +0800

    泛型

--- config/sidebarConf/Javascript/TypeScript.js
+++ config/sidebarConf/Javascript/TypeScript.js
@@ -1,7 +1,7 @@
 const utils = require('../../../utils/index');
 const children = ['什么是TypeScript','原始数据类型','任意值','类型推论','联合类型',
 '对象的类型——接口','数组的类型','函数的类型','类型断言','声明文件','内置对象',
-'进阶','类','类与接口'
+'进阶','类','类与接口','泛型'
 ]
 module.exports =[
     utils.genSidebar('ECMAScript6学习', children, false ),
