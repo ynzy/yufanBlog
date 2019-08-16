331b948f81536ab740198663f774317ab09f180f
zhangyong <zhangyong@163.com>
2019-08-16 13:53:58 +0800

    docker自动部署

--- config/sidebarConf/Others/Git/Git.js
+++ config/sidebarConf/Others/Git/Git.js
@@ -1,5 +1,5 @@
 const utils = require('../../../../utils/index.js');
-const children = ['git基础', 'commitMessage',]
+const children = ['git基础', 'commitMessage','docker和Nginx部署']
 module.exports =[
     utils.genSidebar('git相关知识',children ),
 ];
